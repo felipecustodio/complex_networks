aa154c9442efb1d8718735613ad91dc280a03b7f
Felipe Custódio <felipe.crochi@gmail.com>
2017-11-05 02:48:14 -0200

    Scatter plots and k x knn correlation

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -16,20 +16,21 @@ Assignment 2 - Correlation and Communities
 
 import networkx as nx
 import igraph as ig
+from itertools import *
 import numpy as np
 from matplotlib import pyplot as pp
 import seaborn as sns
 
+# plot helpers
+colors = ["#1abc9c", "#2ecc71", "#3498db", "#f1c40f", "#e67e22", "#e74c3c"]
 
 def pearson(x, y):
-	# from: https://stackoverflow.com/a/5713856
-	# Assume len(x) == len(y)
 	n = len(x)
 	sum_x = float(sum(x))
 	sum_y = float(sum(y))
 	sum_x_sq = sum(map(lambda x: pow(x, 2), x))
 	sum_y_sq = sum(map(lambda x: pow(x, 2), y))
-	psum = sum(imap(lambda x, y: x * y, x, y))
+	psum = sum(map(lambda x, y: x * y, x, y))
 	num = psum - (sum_x * sum_y/n)
 	den = pow((sum_x_sq - pow(sum_x, 2) / n) * (sum_y_sq - pow(sum_y, 2) / n), 0.5)
 	if den == 0: return 0
@@ -59,15 +60,18 @@ def assortativity(graphs):
 	print("ASSORTATIVITY")
 	for name,graph in graphs.items():
 		assortativity = nx.degree_assortativity_coefficient(graph)
-		print("%s,%.4f" % (name, assortativity))
+		print("%s: %.4f" % (name, assortativity))
 
 
 def k_x_knn(graphs):
-	# plot
-	for name, graph in graphs.items():
-		knn = nx.average_degree_connectivity(graph)
-		sns.set()
 
+	print("K X KNN")
+	i = 0
+	for name, graph in graphs.items():
+		print(name)
+		knn = nx.average_degree_connectivity(graph)
+
+		sns.set()
 		# plot k x knn
 		x = list(knn.keys())
 		y = list(knn.values())
@@ -75,40 +79,41 @@ def k_x_knn(graphs):
 		pp.title("k x knn - %s" % name)
 		pp.ylabel("Knn")
 		pp.xlabel("K")
-		pp.scatter(x, y, c=y, s = 100)
+		pp.scatter(x, y, c=colors[i], s = 100)
 		
-		pp.colorbar()
 		pp.grid(False)
-		pp.show()
 
-	# correlation
+		# pp.savefig(name + "-kxknn.png")
+		pp.savefig('graphs/' + name + "-kxknn.png")
+		pp.clf()
+		i += 1
+
+		# correlation
+		correlation = pearson(graph.degree().values(), knn.values())
+		print("correlation: %.4f" % correlation)
 
 
 def modularities(graphs):
 	print("MODULARITIES")
+	
 	for name, graph in graphs.items():
-		print(name)
-		
+		print(name)	
 		# convert to igraph
 		g = nx_to_ig(graph)
-
 		# edge betweenness centrality
 		community = g.community_edge_betweenness(directed=False).as_clustering()
 		print("edge betweenness centrality: %.4f" % (community.modularity))
-
 		# fast-greedy
 		community = g.community_fastgreedy().as_clustering()
 		print("fast greedy: %.4f" % (community.modularity))
-
 		# eigenvectors of matrices
 		community = g.community_leading_eigenvector()
 		print("eigenvectors of matrices: %.4f" % (community.modularity))
-
 		# walktrap
 		community = g.community_walktrap().as_clustering()
 		print("walktrap: %.4f" % (community.modularity))
-
 		print("\n")
+
 
 def plot_modularity_evolution():
 	pass
@@ -129,5 +134,5 @@ graphs["Cortical Monkey"] = read_graph("./networks/cortical-monkey.txt")
 
 # measures
 # assortativity(graphs)
-# plot_knn(graphs)
-modularities(graphs)
+k_x_knn(graphs)
+# modularities(graphs)

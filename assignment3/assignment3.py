7258b24d1cde3d9afbdb3c82f7911b827155f7d5
Felipe Custódio <felipe.crochi@gmail.com>
2017-11-29 23:10:10 -0200

    Assignment 3 - Exercise 1

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -13,6 +13,7 @@ Gabriel Henrique Scalici - 9292970
 
 Assignment 3 - Modelling complex networks, failures and attacks
 '''
+import math
 
 import networkx as nx
 import igraph as ig
@@ -46,6 +47,10 @@ def degree_distribution(graph):
     distribution = sorted(degrees.values())
     return distribution
 
+def average_degree(graph):
+    degrees = graph.degree().values()
+    average = sum(degrees)/len(degrees)
+    return average
 
 def entropy(graph):
     entropy = 0
@@ -73,11 +78,10 @@ def network_models():
     # generate 30 networks of each model
     print("Generating networks...")
     for i in range(30):
-        pass
-        # FIXME
-        #erdos.append(erdos_renyi_graph(n, p[, seed, directed]))
-        #watts.append(watts_strogatz_graph(n, k, p[, seed]))
-        #barabasi.append(barabasi_albert_graph(n, m[, seed]))
+        # usando respectivamente os dados do item 1
+        erdos.append(nx.erdos_renyi_graph(500, 0.1))
+        watts.append(nx.watts_strogatz_graph(1000, 10, 0.1))
+        barabasi.append(nx.barabasi_albert_graph(2000, 10))
 
     # degree distribution (one of each)
     print("Finding degree distributions...")
@@ -91,7 +95,7 @@ def network_models():
     sns.set()
 
     pp.title("Erdös-Rényi - Degree Distribution")
-    pp.hist(erdos[0].degrees(), dists["Erdös-Rényi"], color=colors[0])
+    pp.hist(list(erdos[0].degree().values()), dists["Erdös-Rényi"], color=colors[0])
     pp.ylabel("Frequency")
     pp.xlabel("Degree (k)")
     pp.grid(False)
@@ -99,7 +103,7 @@ def network_models():
     pp.clf()    
 
     pp.title("Watts-Strogatz - Degree Distribution")
-    pp.hist(watts[0].degrees(), dists["Watts-Strogatz"], color=colors[1])
+    pp.hist(list(watts[0].degree().values()), dists["Watts-Strogatz"], color=colors[1])
     pp.ylabel("Frequency")
     pp.xlabel("Degree (k)")
     pp.grid(False)
@@ -107,7 +111,7 @@ def network_models():
     pp.clf()
 
     pp.title("Barabási-Albert - Degree Distribution")
-    pp.hist(barabasi[0].degrees(), dists["Barabási-Albert"], color=colors[2])
+    pp.hist(list(barabasi[0].degree().values()), dists["Barabási-Albert"], color=colors[2])
     pp.ylabel("Frequency")
     pp.xlabel("Degree (k)")
     pp.grid(False)
@@ -118,12 +122,41 @@ def network_models():
     # table
     print("Taking measures...")
     lens = {}
+    lens["erdos"] = []
+    lens["watts"] = []
+    lens["barabasi"] = []
+
     degrees = {}
+    degrees["erdos"] = []
+    degrees["watts"] = []
+    degrees["barabasi"] = []
+
     clusterings = {}
+    clusterings["erdos"] = []
+    clusterings["watts"] = []
+    clusterings["barabasi"] = []
+
     assortativities = {}
+    assortativities["erdos"] = []
+    assortativities["watts"] = []
+    assortativities["barabasi"] = []
+
     shortest_paths = {}
+    shortest_paths["erdos"] = []
+    shortest_paths["watts"] = []
+    shortest_paths["barabasi"] = []
+
     entropies = {}
+    entropies["erdos"] = []
+    entropies["watts"] = []
+    entropies["barabasi"] = []
+
     moments = {}
+    moments["erdos"] = []
+    moments["watts"] = []
+    moments["barabasi"] = []
+
+    print("Calculating Erdös-Rényi measurements")
     for graph in erdos:
         lens["erdos"].append(len(graph))
         degrees["erdos"].append(average_degree(graph))
@@ -133,6 +166,7 @@ def network_models():
         entropies["erdos"].append(entropy(graph))
         moments["erdos"].append(stat_moment(graph, 2))
 
+    print("Calculating Watts-Strogatz measurements")
     for graph in watts:
         lens["watts"].append(len(graph))
         degrees["watts"].append(average_degree(graph))
@@ -142,6 +176,7 @@ def network_models():
         entropies["watts"].append(entropy(graph))
         moments["watts"].append(stat_moment(graph, 2))
 
+    print("Calculating Barabási-Albert measurements")
     for graph in barabasi:
         lens["barabasi"].append(len(graph))
         degrees["barabasi"].append(average_degree(graph))
@@ -154,189 +189,183 @@ def network_models():
     print("Measurements for Erdös-Rényi networks")
     # median
     print("Median of...")
-    print("Number of nodes = %d" % np.median(lens["erdos"]))
-    print("Degrees = %.4f" % np.median(degrees["erdos"]))
-    print("Clustering coefficient = %.4f" % np.median(clusterings["erdos"]))
-    print("Assortativity = %.4f" % np.median(assortativities["erdos"]))
-    print("Shortest paths = %.4f" % np.median(shortest_paths["erdos"]))
-    print("Shannon entropies = %.4f" % np.median(entropies["erdos"]))
-    print("Second stat moments = %.4f" % np.median(moments["erdos"]))
+    print("Number of nodes = %d" % np.median((lens["erdos"])))
+    print("Degrees = %.4f" % np.median((degrees["erdos"])))
+    print("Clustering coefficient = %.4f" % np.median((clusterings["erdos"])))
+    print("Assortativity = %.4f" % np.median((assortativities["erdos"])))
+    print("Shortest paths = %.4f" % np.median((shortest_paths["erdos"])))
+    print("Shannon entropies = %.4f" % np.median((entropies["erdos"])))
+    print("Second stat moments = %.4f" % np.median((moments["erdos"])))
 
     # deviation
     print("Standard Deviation of...")
-    print("Number of nodes = %d" % np.std(lens["erdos"], ddof=1))
-    print("Degrees = %.4f" % np.std(degrees["erdos"], ddof=1))
-    print("Clustering coefficient = %.4f" % np.std(clusterings["erdos"], ddof=1))
-    print("Assortativity = %.4f" % np.std(assortativities["erdos"], ddof=1))
-    print("Shortest paths = %.4f" % np.std(shortest_paths["erdos"], ddof=1))
-    print("Shannon entropies = %.4f" % np.std(entropies["erdos"], ddof=1))
-    print("Second stat moments = %.4f" % np.std(moments["erdos"], ddof=1))
+    print("Number of nodes = %d" % np.std((lens["erdos"]), ddof=1))
+    print("Degrees = %.4f" % np.std((degrees["erdos"]), ddof=1))
+    print("Clustering coefficient = %.4f" % np.std((clusterings["erdos"]), ddof=1))
+    print("Assortativity = %.4f" % np.std((assortativities["erdos"]), ddof=1))
+    print("Shortest paths = %.4f" % np.std((shortest_paths["erdos"]), ddof=1))
+    print("Shannon entropies = %.4f" % np.std((entropies["erdos"]), ddof=1))
+    print("Second stat moments = %.4f" % np.std((moments["erdos"]), ddof=1))
 
     print("Measurements for Watts-Strogatz networks")
     # median
     print("Median of...")
-    print("Number of nodes = %d" % np.median(lens["watts"]))
-    print("Degrees = %.4f" % np.median(degrees["watts"]))
-    print("Clustering coefficient = %.4f" % np.median(clusterings["watts"]))
-    print("Assortativity = %.4f" % np.median(assortativities["watts"]))
-    print("Shortest paths = %.4f" % np.median(shortest_paths["watts"]))
-    print("Shannon entropies = %.4f" % np.median(entropies["watts"]))
-    print("Second stat moments = %.4f" % np.median(moments["watts"]))
+    print("Number of nodes = %d" % np.median((lens["watts"])))
+    print("Degrees = %.4f" % np.median((degrees["watts"])))
+    print("Clustering coefficient = %.4f" % np.median((clusterings["watts"])))
+    print("Assortativity = %.4f" % np.median((assortativities["watts"])))
+    print("Shortest paths = %.4f" % np.median((shortest_paths["watts"])))
+    print("Shannon entropies = %.4f" % np.median((entropies["watts"])))
+    print("Second stat moments = %.4f" % np.median((moments["watts"])))
 
     # deviation
     print("Standard Deviation of...")
-    print("Number of nodes = %d" % np.std(lens["watts"], ddof=1))
-    print("Degrees = %.4f" % np.std(degrees["watts"], ddof=1))
-    print("Clustering coefficient = %.4f" % np.std(clusterings["watts"], ddof=1))
-    print("Assortativity = %.4f" % np.std(assortativities["watts"], ddof=1))
-    print("Shortest paths = %.4f" % np.std(shortest_paths["watts"], ddof=1))
-    print("Shannon entropies = %.4f" % np.std(entropies["watts"], ddof=1))
-    print("Second stat moments = %.4f" % np.std(moments["watts"], ddof=1))
+    print("Number of nodes = %d" % np.std((lens["watts"]), ddof=1))
+    print("Degrees = %.4f" % np.std((degrees["watts"]), ddof=1))
+    print("Clustering coefficient = %.4f" % np.std((clusterings["watts"]), ddof=1))
+    print("Assortativity = %.4f" % np.std((assortativities["watts"]), ddof=1))
+    print("Shortest paths = %.4f" % np.std((shortest_paths["watts"]), ddof=1))
+    print("Shannon entropies = %.4f" % np.std((entropies["watts"]), ddof=1))
+    print("Second stat moments = %.4f" % np.std((moments["watts"]), ddof=1))
 
     print("Measurements for Barabási-Albert networks")
     # median
     print("Median of...")
-    print("Number of nodes = %d" % np.median(lens["barabasi"]))
-    print("Degrees = %.4f" % np.median(degrees["barabasi"]))
-    print("Clustering coefficient = %.4f" % np.median(clusterings["barabasi"]))
-    print("Assortativity = %.4f" % np.median(assortativities["barabasi"]))
-    print("Shortest paths = %.4f" % np.median(shortest_paths["barabasi"]))
-    print("Shannon entropies = %.4f" % np.median(entropies["barabasi"]))
-    print("Second stat moments = %.4f" % np.median(moments["barabasi"]))
+    print("Number of nodes = %d" % np.median((lens["barabasi"])))
+    print("Degrees = %.4f" % np.median((degrees["barabasi"])))
+    print("Clustering coefficient = %.4f" % np.median((clusterings["barabasi"])))
+    print("Assortativity = %.4f" % np.median((assortativities["barabasi"])))
+    print("Shortest paths = %.4f" % np.median((shortest_paths["barabasi"])))
+    print("Shannon entropies = %.4f" % np.median((entropies["barabasi"])))
+    print("Second stat moments = %.4f" % np.median((moments["barabasi"])))
 
     # deviation
     print("Standard Deviation of...")
-    print("Number of nodes = %d" % np.std(lens["barabasi"], ddof=1))
-    print("Degrees = %.4f" % np.std(degrees["barabasi"], ddof=1))
-    print("Clustering coefficient = %.4f" % np.std(clusterings["barabasi"], ddof=1))
-    print("Assortativity = %.4f" % np.std(assortativities["barabasi"], ddof=1))
-    print("Shortest paths = %.4f" % np.std(shortest_paths["barabasi"], ddof=1))
-    print("Shannon entropies = %.4f" % np.std(entropies["barabasi"], ddof=1))
-    print("Second stat moments = %.4f" % np.std(moments["barabasi"], ddof=1))
+    print("Number of nodes = %d" % np.std((lens["barabasi"]), ddof=1))
+    print("Degrees = %.4f" % np.std((degrees["barabasi"]), ddof=1))
+    print("Clustering coefficient = %.4f" % np.std((clusterings["barabasi"]), ddof=1))
+    print("Assortativity = %.4f" % np.std((assortativities["barabasi"]), ddof=1))
+    print("Shortest paths = %.4f" % np.std((shortest_paths["barabasi"]), ddof=1))
+    print("Shannon entropies = %.4f" % np.std((entropies["barabasi"]), ddof=1))
+    print("Second stat moments = %.4f" % np.std((moments["barabasi"]), ddof=1))
 
 # 2
-def ER_model():
-    giants = {}
-    degrees = range(0,5)
-    int p = 0
+# def ER_model():
+#     giants = {}
+#     degrees = range(0,5)
     
-    print("Finding giant components for different average degrees...")
-    for p in degrees:
-        # generate ER networks
-        current = erdos_renyi_graph(1000, p)
-        # store size of giant component for current p
-        giants[p] = len(giant_component(current))
+#     print("Finding giant components for different average degrees...")
+#     for p in degrees:
+#         # generate ER networks
+#         current = erdos_renyi_graph(1000, p)
+#         # store size of giant component for current p
+#         giants[p] = len(giant_component(current))
     
-    # plot
-    print("Plotting...")
-    sns.set()
-    pp.plot(degrees, giants.values(), color=colors[0])
-    pp.xlabel("Average degree")
-    pp.ylabel("Size of giant component")
-    pp.grid(False)
-    pp.savefig('plots/ER-evolution.png')
-    pp.clf()    
+#     # plot
+#     print("Plotting...")
+#     sns.set()
+#     pp.plot(degrees, giants.values(), color=colors[0])
+#     pp.xlabel("Average degree")
+#     pp.ylabel("Size of giant component")
+#     pp.grid(False)
+#     pp.savefig('plots/ER-evolution.png')
+#     pp.clf()    
 
-# 3 
-def WS_model():
-    clusterings = []
-    paths = []
-    # generate WS networks
-    for p in range(0,?): # FIXME
-        network = watts_strogatz_graph(1000, 5, p) # arbitrary values for N and K so far
-        clusterings.append(nx.average_clustering(network))
-        paths.append(nx.average_shortest_path_length(network))
+# # 3 
+# def WS_model():
+#     clusterings = []
+#     paths = []
+#     # generate WS networks
+#     for p in range(0,?): # FIXME
+#         network = watts_strogatz_graph(1000, 5, p) # arbitrary values for N and K so far
+#         clusterings.append(nx.average_clustering(network))
+#         paths.append(nx.average_shortest_path_length(network))
 
-    # plot
-    x = range(0, ?)
-    y1 = clusterings
-    y2 = paths
+#     # plot
+#     x = range(0, ?)
+#     y1 = clusterings
+#     y2 = paths
 
-    # we have to use subplots
+#     # we have to use subplots
 
-# 4 
-def BA_model():
-    # generate 30 networks, do the same as one
-    for power in range(0,?):
-        # generate Barabasi network with p = power
+# # 4 
+# def BA_model():
+#     # generate 30 networks, do the same as one
+#     for power in range(0,?):
+#         # generate Barabasi network with p = power
 
 
-
-# 5
-def stress_test():
-    # random removals
-    lens = {}
-    erdos = nx.erdos_renyi_graph()
-    lens["erdos"] = len(erdos)
-    barabasi = nx.barabasi_albert_graph()
-    lens["barabasi"] = len(barabasi)
+# # 5
+# def stress_test():
+#     # random removals
+#     lens = {}
+#     erdos = nx.erdos_renyi_graph()
+#     lens["erdos"] = len(erdos)
+#     barabasi = nx.barabasi_albert_graph()
+#     lens["barabasi"] = len(barabasi)
  
-    giant_sizes = {}
-    while (len(erdos) > 0):
-        # remove vertex
-        giant_sizes["erdos"].append(len(giant_component(erdos)))
+#     giant_sizes = {}
+#     while (len(erdos) > 0):
+#         # remove vertex
+#         giant_sizes["erdos"].append(len(giant_component(erdos)))
 
-    while (len(barabasi) > 0):
-        # remove vertex
-        giant_sizes["barabasi"].append(len(giant_component(barabasi)))
+#     while (len(barabasi) > 0):
+#         # remove vertex
+#         giant_sizes["barabasi"].append(len(giant_component(barabasi)))
 
-    # plot stress test
-    sns.set()
-    pp.plot(lens["erdos"], giants_sizes["erdos"].values(), color=colors[0])
-    pp.xlabel("Number of removals")
-    pp.ylabel("Size of giant component")
-    pp.grid(False)
-    pp.savefig('plots/ER-stress-test.png')
-    pp.clf() 
+#     # plot stress test
+#     sns.set()
+#     pp.plot(lens["erdos"], giants_sizes["erdos"].values(), color=colors[0])
+#     pp.xlabel("Number of removals")
+#     pp.ylabel("Size of giant component")
+#     pp.grid(False)
+#     pp.savefig('plots/ER-stress-test.png')
+#     pp.clf() 
 
-    sns.set()
-    pp.plot(lens["barabasi"], giants_sizes["barabasi"].values(), color=colors[0])
-    pp.xlabel("Number of removals")
-    pp.ylabel("Size of giant component")
-    pp.grid(False)
-    pp.savefig('plots/BA-stress-test.png')
-    pp.clf() 
+#     sns.set()
+#     pp.plot(lens["barabasi"], giants_sizes["barabasi"].values(), color=colors[0])
+#     pp.xlabel("Number of removals")
+#     pp.ylabel("Size of giant component")
+#     pp.grid(False)
+#     pp.savefig('plots/BA-stress-test.png')
+#     pp.clf() 
 
-    # remove from most connected to least connected
-    lens.clear()
-    erdos = nx.erdos_renyi_graph()
-    lens["erdos"] = len(erdos)
-    barabasi = nx.barabasi_albert_graph()
-    lens["barabasi"] = len(barabasi)
+#     # remove from most connected to least connected
+#     lens.clear()
+#     erdos = nx.erdos_renyi_graph()
+#     lens["erdos"] = len(erdos)
+#     barabasi = nx.barabasi_albert_graph()
+#     lens["barabasi"] = len(barabasi)
 
-    giant_sizes.clear()
-    while (len(erdos) > 0):
-        # remove vertex
-        giant_sizes["erdos"].append(len(giant_component(erdos)))
+#     giant_sizes.clear()
+#     while (len(erdos) > 0):
+#         # remove vertex
+#         giant_sizes["erdos"].append(len(giant_component(erdos)))
 
-    while (len(barabasi) > 0):
-        # remove vertex
-        giant_sizes["barabasi"].append(len(giant_component(barabasi)))
+#     while (len(barabasi) > 0):
+#         # remove vertex
+#         giant_sizes["barabasi"].append(len(giant_component(barabasi)))
 
-    # plot stress test
-    sns.set()
-    pp.plot(lens["erdos"], giants_sizes["erdos"].values(), color=colors[0])
-    pp.xlabel("Number of removals")
-    pp.ylabel("Size of giant component")
-    pp.grid(False)
-    pp.savefig('plots/ER-stress-test.png')
-    pp.clf() 
+#     # plot stress test
+#     sns.set()
+#     pp.plot(lens["erdos"], giants_sizes["erdos"].values(), color=colors[0])
+#     pp.xlabel("Number of removals")
+#     pp.ylabel("Size of giant component")
+#     pp.grid(False)
+#     pp.savefig('plots/ER-stress-test.png')
+#     pp.clf() 
 
-    sns.set()
-    pp.plot(lens["barabasi"], giants_sizes["barabasi"].values(), color=colors[0])
-    pp.xlabel("Number of removals")
-    pp.ylabel("Size of giant component")
-    pp.grid(False)
-    pp.savefig('plots/BA-stress-test.png')
-    pp.clf() 
+#     sns.set()
+#     pp.plot(lens["barabasi"], giants_sizes["barabasi"].values(), color=colors[0])
+#     pp.xlabel("Number of removals")
+#     pp.ylabel("Size of giant component")
+#     pp.grid(False)
+#     pp.savefig('plots/BA-stress-test.png')
+#     pp.clf() 
 
 
 def main():
-    # one()
-    # two()
-    # three()
-    # four()
-    # five()
+    network_models()
     print("done")
 
 if __name__ == "__main__":
